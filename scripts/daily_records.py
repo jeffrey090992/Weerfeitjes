b8480f4a72ed22d76d3b80c870b458aecf96ef6d
jeffrey090992 <jeffreynijenhuis2@gmail.com>
2026-07-12 21:46:41 +0200

    Update daily_records.py

--- scripts/daily_records.py
+++ scripts/daily_records.py
@@ -18,8 +18,8 @@ def zoek_record(kolom, volgorde):
     cursor.execute(f"""
         SELECT datum, station, {kolom}
         FROM measurements
-        WHERE substr(datum,5,2)=?
-        AND substr(datum,7,2)=?
+        WHERE substr(CAST(datum AS TEXT), 5, 2) = ?
+        AND substr(CAST(datum AS TEXT), 7, 2) = ?
         ORDER BY {kolom} {volgorde}
         LIMIT 1
     """, (maand, dag))
@@ -31,30 +31,29 @@ warmste = zoek_record("temp_max", "DESC")
 koudste = zoek_record("temp_min", "ASC")
 natste = zoek_record("regen", "DESC")
 
-
 conn.close()
+
+
+def maak_record(data):
+    if data:
+        return {
+            "datum": data[0],
+            "station": data[1],
+            "waarde": data[2]
+        }
+    else:
+        return {
+            "datum": "geen data",
+            "station": "onbekend",
+            "waarde": None
+        }
 
 
 records = {
     "datum": f"{dag}-{maand}",
-
-    "warmste": {
-        "datum": warmste[0],
-        "station": warmste[1],
-        "waarde": warmste[2]
-    },
-
-    "koudste": {
-        "datum": koudste[0],
-        "station": koudste[1],
-        "waarde": koudste[2]
-    },
-
-    "natste": {
-        "datum": natste[0],
-        "station": natste[1],
-        "waarde": natste[2]
-    }
+    "warmste": maak_record(warmste),
+    "koudste": maak_record(koudste),
+    "natste": maak_record(natste)
 }
 
 
